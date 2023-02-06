543af4f328b302334c6eeb2779077429832707bf
AllanRicksr <allanrickrodrigues@hotmail.com>
2023-02-06 14:55:17 -0400

    CloneFlix-First-commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "7a03797ec2018ed508885fbca7b5b449"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
